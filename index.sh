21f03d155f640bba473a861140c3b4e7054fe312
MefelemS <72390888+MefelemS@users.noreply.github.com>
2021-05-22 13:17:39 +0700

    index.sh

---
+++ index.sh
@@ -0,0 +1,14 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+toilet -f normal -F gay Z-Script
+echo $yellow"Author: Meflems"
+echo 
+echo $blue"Fiture:"
+echo $green"[1] Install Web Server"
+read -p "asu: " ajg
